d687d81954ac927fbadf898b37c04ded7b789a5a
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-12-13 16:22:43 -0500

    Added the skeleton html, a css and js page with basic outline for functions and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is for the practice of object construction and prototype inheritance in javascript.  Will also be pleasant review of concepts covered in the earlier "foundations" course.
---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function book(title, author, pageCount, pagesRead) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.pagesRead = pagesRead
+}
+
+function addBookToLibrary() {
+}
