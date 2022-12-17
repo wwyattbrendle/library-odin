5be1efe7f323e66ad11967b57f8f7e3468d5eb3d
Wyatt Brendle <accounts@wyattbrendle.tech>
2022-12-16 20:17:02 -0500

    Almost completed the javascript, added functionality for all buttons, just running into a bug one the delete button

--- script.js
+++ script.js
@@ -1,109 +1,180 @@
 let myLibrary = [];
 
-function book(title, author, pageCount, pagesRead) {
+function Book(title, author, pageCount, pagesRead) {
     this.title = title;
     this.author = author;
-    this.pageCount = pageCount;
-    this.pagesRead = pagesRead
+    this.pageCount = parseInt(pageCount);
+    this.pagesRead = parseInt(pagesRead);
+    if (pageCount === pagesRead) {
+        this.isRead = true;
+    } else {
+        this.isRead = false;
+    }
+    this.isAppended = false;
 }
+
+const a = document.querySelector("#title");
+const b = document.querySelector("#author");
+const c = document.querySelector("#page-count");
+const d = document.querySelector("#pages-read");
 
 function addBookToLibrary() {
-
+    console.log(a.value, b.value, c.value, d.value);
+    let newBook = new Book(a.value, b.value, c.value, d.value);
+    myLibrary.push(newBook);
+    a.value = "";
+    b.value = "";
+    c.value = "";
+    d.value = "";
 }
 
-let title = "Example Title"
-let author = "Example Author"
-let currentPage = 500;
-let totalPages = 1000;
-
-//all elements are to match the index.html
-//create basic elements to call upon later
-const bookElement = document.createElement("div")
-bookElement.classList.add("book");
-const titleElement = document.createElement("h4");
-const authorElement = document.createElement("h5");
-const currentPageElement = document.createElement("h5");
-const incrementButton = document.createElement("button")
-incrementButton.classList.add("increment");
-const decrementButton = document.createElement("button")
-decrementButton.classList.add("decrement");
-const pageTotalElement = document.createElement("h6")
-pageTotalElement.classList.add("total-pages");
-
-//add text to elements
-titleElement.textContent = title;
-authorElement.textContent = author;
-currentPageElement.textContent = `Page ${currentPage}`;
-pageTotalElement.textContent = `of ${totalPages}`
-
-//add text for buttons
-incrementButton.innerHTML = '<p class="button-text">+</p>';
-decrementButton.innerHTML = '<p class="button-text">-</p>';
-
-//create two containers for various elements
-const pageButtonElement = document.createElement("div")
-pageButtonElement.classList.add("page-buttons");
-const pageContainer = document.createElement("div")
-pageContainer.classList.add("page");
-
-//append buttons to container
-pageButtonElement.appendChild(incrementButton);
-pageButtonElement.appendChild(decrementButton);
-
-//append current page and button container to another div
-pageContainer.appendChild(currentPageElement);
-pageContainer.appendChild(pageButtonElement);
-
-//start appending to book
-bookElement.appendChild(titleElement);
-bookElement.appendChild(authorElement);
-bookElement.appendChild(pageContainer);
-bookElement.appendChild(pageTotalElement);
-
-//create div for second half of book html
-const buttonsContainer = document.createElement("div")
-buttonsContainer.classList.add("book-buttons");
-const deleteButton = document.createElement("button")
-deleteButton.classList.add("delete-button");
-const switchContainer = document.createElement("div")
-switchContainer.classList.add("my-label");
-
-//create elements for switch
-const checkBox = document.createElement("input")
-checkBox.setAttribute("id", "switch1");
-checkBox.setAttribute("name", "switch");
-checkBox.setAttribute("type", "checkbox");
-const groove = document.createElement("div")
-groove.classList.add("sliding-groove");
-const labelElement = document.createElement("label")
-labelElement.classList.add("my-label")
-labelElement.setAttribute("for", "switch1");
-const spanElement = document.createElement("span")
-spanElement.classList.add("labels")
-spanElement.setAttribute("data-on", "READ!");
-spanElement.setAttribute("data-off", "");
-
-//add text content
-deleteButton.textContent = "Delete";
-
-
-//begin appending to secondary container div
-switchContainer.appendChild(checkBox);
-switchContainer.appendChild(groove);
-switchContainer.appendChild(labelElement);
-switchContainer.appendChild(spanElement);
-
-//append to button div container
-buttonsContainer.appendChild(deleteButton);
-buttonsContainer.appendChild(switchContainer);
-
-//append button container to book
-bookElement.appendChild(buttonsContainer);
-
+//container to append books
 const contentContainer = document.getElementById("append-here");
-contentContainer.appendChild(bookElement);
+
+
+function createBookElement (arrayPosition, title, author, totalPages, currentPage) { 
+    let n = arrayPosition;
+
+    //create basic elements to call upon later
+    const bookElement = document.createElement("div")
+    bookElement.classList.add("book");
+    bookElement.setAttribute("id", `${n}`);
+    const titleElement = document.createElement("h4");
+    const authorElement = document.createElement("h5");
+    const currentPageElement = document.createElement("h5");
+    const incrementButton = document.createElement("button")
+    incrementButton.classList.add("increment");
+    const decrementButton = document.createElement("button")
+    decrementButton.classList.add("decrement");
+    const pageTotalElement = document.createElement("h6")
+    pageTotalElement.classList.add("total-pages");
+
+    //add text to elements
+    titleElement.textContent = title;
+    authorElement.textContent = author;
+    currentPageElement.textContent = `Page ${currentPage}`;
+    pageTotalElement.textContent = `of ${totalPages}`
+
+    //add text for buttons
+    incrementButton.innerHTML = '<p class="button-text">+</p>';
+    decrementButton.innerHTML = '<p class="button-text">-</p>';
+
+    //create two containers for various elements
+    const pageButtonElement = document.createElement("div")
+    pageButtonElement.classList.add("page-buttons");
+    const pageContainer = document.createElement("div")
+    pageContainer.classList.add("page");
+
+    //append buttons to container
+    pageButtonElement.appendChild(incrementButton);
+    pageButtonElement.appendChild(decrementButton);
+
+    //append current page and button container to another div
+    pageContainer.appendChild(currentPageElement);
+    pageContainer.appendChild(pageButtonElement);
+
+    //start appending to book
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(authorElement);
+    bookElement.appendChild(pageContainer);
+    bookElement.appendChild(pageTotalElement);
+
+    //create div for second half of book html
+    const buttonsContainer = document.createElement("div")
+    buttonsContainer.classList.add("book-buttons");
+    const deleteButton = document.createElement("button")
+    deleteButton.classList.add("delete-button");
+    const switchContainer = document.createElement("div")
+    switchContainer.classList.add("my-label");
+
+    //create elements for switch
+    const checkBox = document.createElement("input")
+    checkBox.setAttribute("id", `switch-${n}`);
+    checkBox.setAttribute("name", "switch");
+    checkBox.setAttribute("type", "checkbox");
+    const groove = document.createElement("div")
+    groove.classList.add("sliding-groove");
+    const labelElement = document.createElement("label")
+    labelElement.classList.add("my-label")
+    labelElement.setAttribute("for", `switch-${n}`);
+    const spanElement = document.createElement("span")
+    spanElement.classList.add("labels")
+    spanElement.setAttribute("data-on", "READ!");
+    spanElement.setAttribute("data-off", "");
+
+    //add text content
+    deleteButton.textContent = "Delete";
+
+
+    //begin appending to secondary container div
+    switchContainer.appendChild(checkBox);
+    switchContainer.appendChild(groove);
+    switchContainer.appendChild(labelElement);
+    switchContainer.appendChild(spanElement);
+
+    //append to button div container
+    buttonsContainer.appendChild(deleteButton);
+    buttonsContainer.appendChild(switchContainer);
+
+    //append button container to book
+    bookElement.appendChild(buttonsContainer);
+    contentContainer.appendChild(bookElement);
+
+    //add event listeners to buttons
+    incrementButton.addEventListener("click", () => {
+        if (currentPage < totalPages) {
+            currentPage++;
+            if (currentPage === totalPages) {
+                checkBox.click();
+            }
+        }
+        currentPageElement.textContent = `Page ${currentPage}`;
+    });
+
+    decrementButton.addEventListener("click", () => {
+        if (currentPage === totalPages && bookElement.classList.contains("read")) {
+            currentPage--;
+            checkBox.click();
+        } else if (currentPage > 0) {
+            currentPage--;
+        }
+        currentPageElement.textContent = `Page ${currentPage}`;
+    });
+
+    checkBox.addEventListener("click", () => {
+        bookElement.classList.toggle("read");
+    });
+//                                                               //
+//\                                                             ///
+//\\                                                           ////
+//\\\                                                         /////
+//\\\\                                                       //////
+//\\\\\                                                     ///////
+//\\\\\\                                                   ////////
+////////            /////  COMPLETE BELOW  \\\\\           \\\\\\\\
+///////                                                     \\\\\\\
+//////                                                       \\\\\\
+/////                                                         \\\\\
+////                                                           \\\\
+///                                                             \\\
+//                                                               \\
+    deleteButton.addEventListener("click", () => {
+        if (confirm("Are you sure you want to delete this book? This action is irreversible") == true) {
+            contentContainer.remove(bookElement);
+            // delete myLibrary[n];
+        }
+    });
+}
 
 const addButton = document.getElementById("add-button")
-addButton.addEventListener("click", () => {
-    contentContainer.appendChild(bookElement);
+
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].isAppended === false) {
+            createBookElement(i, myLibrary[i].title, myLibrary[i].author, myLibrary[i].pageCount, myLibrary[i].pagesRead);
+            myLibrary[i].isAppended = true;
+        }
+    }
 });
